d0d599075866f27896df3061f055ceee9c697d33
Myystigri <dorian.walck@gmail.com>
2016-03-24 10:30:40 +0100

    passage vers php

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			<link type="text/css" rel="stylesheet" href="./css/bootstrap.min.css"></link>
+			<link type="text/css" rel="stylesheet" href="./css/style.css"></link>
+			<title>Point Jardin ǀ Matériaux pour le Jardin ǀ Saint Rémy sur Avre</title>
+			<meta charset="utf-8"></meta>
+			<link type="image/jpg" href="https://static.wixstatic.com/media/d968a4_61fe7541a8bd471ba9468f083e604d50.jpg" rel="shortcut icon"></link>
+			<link type="image/jpg" href="https://static.wixstatic.com/media/d968a4_61fe7541a8bd471ba9468f083e604d50.jpg" rel="apple-touch-icon"></link>
+		</head>
+
+		<body>
+			<div class ="logo">
+				<a id="home" href="#"><img title ="Point Jardin" alt="Point Jardin" src="logo.jpg"/></a>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="#"><b>ACCUEIL</a></li></b>
+					<li><a href="#"><b>NOS PRODUITS</a></li></b>
+					<li><a href="#"><b>NOS SERVICES</a></li></b>
+					<li><a href="#"><b>QUI SOMMES NOUS?</a></li></b>
+					<li><a href="#"><b>CONTACT</a></li></b>
+				</ul>
+			</nav>
+			
+
+			<div class="activities">
+				
+				<div class="col-lg-offset-2 col-lg-8">
+					<div class="presentation">
+						<div class="cadre">
+							<h1><b>BIENVENUE CHEZ POINT JARDIN !</b></h1>
+						</div>
+						<h2><b>Paul et Laurentine vous accueillent dans votre nouveau point de vente de matériaux pour l’aménagement du jardin.</b></h2>
+					</div>
+				</div>
+				<div class="col-lg-12">
+					<div class="col-lg-offset-3 col-lg-6 parts">
+						<img class="ico" src="camion.png"/>
+						<p><b>Pas de véhicule adapté? Point jardin vous livre chez vous, petite ou grande quantité</b><p>
+					</div>
+				</div>
+				<div class="col-lg-12">
+					<div class="col-lg-offset-3 col-lg-6 parts">
+						<img class="ico" src="idea.png"/>
+						<p><b>En panne d’inspiration? Nous vous conseillons et dessinons votre jardin</b><p>
+					</div>
+				</div>
+				<div class="col-lg-12">
+					<div class="col-lg-offset-3 col-lg-6 parts">
+						<img class="ico" src="charts.png"/>
+						<p><b>Achetez la quantité dont vous avez besoin</b><p>
+					</div>
+					<div class="col-lg-offset-3 col-lg-6 parts">
+						<img class="ico" src="flower.png"/>
+						<p><b>Embellisez votre jardin avec notre gamme de paillage variés</b><p>
+					</div>
+				</div>
+			</div>
+    		<div class="col-lg-12 footer">
+    			<div class="logos-index">
+      				<a href="error.html"><img id="cheese" src="instagram1.png"/></a>
+      				<a href="https://www.facebook.com/pages/Point-Jardin/861988743863661"><img id="zuckerberg" src="facebook1.png"/></a>
+    			</div>
+				<p id = "footer-index">&copy; Point Jardin 2016 - <b id="number">02.37.31.35.34</b></p>
+			</div>
+		</body>
+	</html>
